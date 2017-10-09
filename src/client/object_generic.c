77754d4b82133852f78e1dfebbee36c92b1bc66b
Daisuke Baba <baba.daisuke@gmail.com>
2017-10-09 21:56:19 +0900

    Add read operation function and basic IPC functions
    write/discover/execute/delete/create operations are not yet supported at this time.

--- src/client/object_generic.c
+++ src/client/object_generic.c
@@ -17,6 +17,8 @@
 
 #include "liblwm2m.h"
 #include "lwm2mclient.h"
+#include "base64.h"
+#include "commandline.h"
 
 #include <string.h>
 #include <stdlib.h>
@@ -33,10 +35,106 @@
 #include <errno.h>
 #include <signal.h>
 
+#define MAX_MESSAGE_SIZE 65536
+
 typedef struct
 {
-    uint8_t objectId;
+    uint16_t objectId;
+    uint8_t * response;
+    size_t responseLen;
 } parent_context_t;
+
+static uint8_t * find_base64_from_response(char * cmd, uint8_t * resp)
+{
+    uint16_t i = 0;
+    uint16_t size = 5 + strlen(cmd) + 1; // without \0
+    // /resp:{command}:{base64 payload}\r\n
+    uint8_t * expected = lwm2m_malloc(size);
+    uint8_t * ptr = expected;
+    strcpy((char *)ptr, "/resp:");
+    ptr += 6; // strlen("/resp:")
+    strcpy((char *)ptr, cmd);
+    ptr += strlen(cmd);
+    *ptr = ':';
+    while ((i < size) && (*expected == *resp)) {
+        ++expected;
+        ++resp;
+        ++i;
+    }
+    if (i != size) {
+        return NULL;
+    }
+    return resp + 1; // next to ':'
+}
+
+static uint8_t request_command(parent_context_t * context,
+                               char * cmd,
+                               uint8_t * payloadRaw,
+                               size_t payloadRawLen)
+{
+    fd_set readfds;
+    struct timeval tv;
+    size_t payloadLen;
+    uint8_t * payload;
+    int recvResult;
+    uint8_t buffer[MAX_MESSAGE_SIZE];
+    size_t recvLen;
+
+    // parent process re timeout
+    tv.tv_sec = 1;       // 1sec
+    tv.tv_usec = 500000; // 500ms
+
+    // setup FD
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    // encode payload
+    payload = util_base64_encode(
+        (const uint8_t *)payloadRaw, payloadRawLen, &payloadLen);
+    if (NULL == payload) {
+        fprintf(stderr, "error:COAP_400_BAD_REQUEST=>[%s]\r\n", cmd);
+        return COAP_400_BAD_REQUEST;
+    }
+
+    // send command
+    fprintf(stdout, "/%s:%s\r\n", cmd, payload);
+    fflush(stdout);
+
+    // release
+    lwm2m_free(payload);
+    payload = NULL;
+
+    // wait for response
+    recvResult = select(FD_SETSIZE, &readfds, NULL, NULL, &tv);
+    if (recvResult < 1 || !FD_ISSET(STDIN_FILENO, &readfds)) {
+        fprintf(stderr, "error:COAP_501_NOT_IMPLEMENTED=>[%s]\r\n", cmd);
+        return COAP_501_NOT_IMPLEMENTED;
+    }
+
+    // read recv data
+    recvLen = read(STDIN_FILENO, buffer, MAX_MESSAGE_SIZE - 1);
+    if (recvLen < 1) {
+        fprintf(stderr, "error:COAP_500_INTERNAL_SERVER_ERROR=>[%s], empty response\r\n", cmd);
+        return COAP_500_INTERNAL_SERVER_ERROR;
+    }
+    buffer[recvLen] = '\0';
+    payload = find_base64_from_response(cmd, buffer);
+    if (NULL == payload) {
+        fprintf(stderr, "error:COAP_500_INTERNAL_SERVER_ERROR=>[%s], resp=>[%s]\r\n", cmd, buffer);
+        return COAP_500_INTERNAL_SERVER_ERROR;
+    }
+    payloadLen = strlen((const char *)payload);
+
+    // decoded result
+    fprintf(stderr, "done:cmd=>[%s], resp=>[%s], base64=>[%s], base64Len=>[%zu]\r\n", cmd, buffer, payload, payloadLen);
+    context->response = util_base64_decode(payload, payloadLen, &context->responseLen);
+    if (context->responseLen == 0) {
+        fprintf(stderr, "error:COAP_500_INTERNAL_SERVER_ERROR=>[%s], resp=>[%s]\r\n", cmd, buffer);
+        return COAP_500_INTERNAL_SERVER_ERROR;
+    }
+
+    return COAP_NO_ERROR;
+}
 
 static parent_context_t * setup_parent_context(uint8_t objectId)
 {
@@ -46,14 +144,135 @@ static parent_context_t * setup_parent_context(uint8_t objectId)
     return context;
 }
 
+static void response_free(parent_context_t * context)
+{
+    if (context->response) {
+        lwm2m_free(context->response);
+        context->response = NULL;
+    }
+    context->responseLen = 0;
+}
+
+static void lwm2m_data_cp(lwm2m_data_t * dataP,
+                          uint8_t * data,
+                          uint16_t len)
+{
+    char * buf;
+    switch(dataP->type) {
+        case LWM2M_TYPE_STRING:
+        case LWM2M_TYPE_OPAQUE:
+            dataP->value.asBuffer.buffer = lwm2m_malloc(len);
+            dataP->value.asBuffer.length = len;
+            memcpy(dataP->value.asBuffer.buffer, data, len);
+            break;
+        case LWM2M_TYPE_INTEGER:
+            buf = lwm2m_malloc(len + 1);
+            memcpy(buf, data, len);
+            buf[len] = '\0';
+            dataP->value.asInteger = strtoll(buf, NULL, 10);
+            lwm2m_free(buf);
+            break;
+        case LWM2M_TYPE_FLOAT:
+            buf = lwm2m_malloc(len + 1);
+            memcpy(buf, data, len);
+            buf[len] = '\0';
+            dataP->value.asFloat = strtod(buf, NULL);
+            lwm2m_free(buf);
+            break;
+        case LWM2M_TYPE_BOOLEAN:
+            dataP->value.asBoolean = (data[0] == 1);
+            break;
+        case LWM2M_TYPE_OBJECT_LINK:
+            dataP->value.asObjLink.objectId = data[0] + (((uint16_t)data[1]) << 8);
+            dataP->value.asObjLink.objectInstanceId = data[2] + (((uint16_t)data[3]) << 8);
+            break;
+        default:
+            break;
+    }
+}
+
 static uint8_t prv_generic_read(uint16_t instanceId,
                                 int * numDataP,
                                 lwm2m_data_t ** dataArrayP,
                                 lwm2m_object_t * objectP)
 {
-    uint8_t result = 0;
+    if (*numDataP > 255) {
+        return COAP_400_BAD_REQUEST;
+    }
+
+    uint16_t i = 0;
+    uint8_t messageId = 0x01;
+    uint8_t result;
     parent_context_t * context = (parent_context_t *)objectP->userData;
-    // TODO
+    size_t payloadRawLen = 8 + *numDataP * 2;
+    uint8_t * payloadRaw = lwm2m_malloc(payloadRawLen);
+    payloadRaw[i++] = 0x01;                     // Data Type: 0x01 (Request), 0x02 (Response)
+    payloadRaw[i++] = messageId;                // Message Id associated with Data Type
+    payloadRaw[i++] = context->objectId & 0xff; // ObjectID LSB
+    payloadRaw[i++] = context->objectId >> 8;   // ObjectID MSB
+    payloadRaw[i++] = instanceId & 0xff;        // InstanceId LSB
+    payloadRaw[i++] = instanceId >> 8;          // InstanceId MSB
+    payloadRaw[i++] = *numDataP & 0xff;         // # of required data LSB (0x0000=ALL)
+    payloadRaw[i++] = *numDataP >> 8;           // # of required data MSB
+    for(; i < payloadRawLen;) {
+        uint16_t id = (*dataArrayP)[i - 8].id;
+        payloadRaw[i++] = id & 0xff; // ResourceId LSB
+        payloadRaw[i++] = id >> 8;   // ResourceId MSB
+    }
+
+    fprintf(stderr, "prv_generic_read:objectId=>%hu, instanceId=>%hu, numDataP=>%d\r\n",
+        context->objectId, instanceId, *numDataP);
+    result = request_command(context, "read", payloadRaw, payloadRawLen);
+    lwm2m_free(payloadRaw);
+
+    /*
+     * Response Data Format (result = COAP_NO_ERROR)
+     * 02 ... Data Type: 0x01 (Request), 0x02 (Response)
+     * 00 ... Message Id associated with Data Type
+     * 45 ... Result Status Code e.g. COAP_205_CONTENT
+     * 00 ... ObjectID LSB
+     * 00 ... ObjectID MSB
+     * 00 ... InstanceId LSB
+     * 00 ... InstanceId MSB
+     * 00 ... # of resources LSB
+     * 00 ... # of resources MSB
+     * 00 ... ResouceId LSB  <============= First ResourceId LSB (index:9)
+     * 00 ... ResouceId MSB
+     * 00 ... Resouce Data Type
+     * 00 ... Length of resource data LSB
+     * 00 ... Length of resource data MSB
+     * 00 ... Resource Data
+     * 00 ... ResouceId LSB  <============= Second ResourceId LSB
+     * 00 ... ResouceId MSB
+     * 00 ... Resouce Data Type
+     * 00 ... Length of resource data LSB
+     * 00 ... Length of resource data MSB
+     * 00 ... Resource Data
+     * ..
+     */
+    uint16_t idx = 9; // First ResouceId LSB index
+    uint16_t len; // Resource data length
+    uint8_t * response = context->response;
+    if (COAP_NO_ERROR == result && response[0] == 0x02 && messageId == response[1]) {
+        result = response[2];
+        if (*numDataP == 0) {
+            *numDataP = response[7] + (((uint16_t)response[8]) << 8);
+            *dataArrayP = lwm2m_data_new(*numDataP);
+            if (*dataArrayP == NULL) return COAP_500_INTERNAL_SERVER_ERROR;
+            fprintf(stderr, "prv_generic_read:(lwm2m_data_new):*numDataP=>%d\r\n",
+                *numDataP);
+        }
+        for (i = 0; i < *numDataP; i++) {
+            (*dataArrayP)[i].id = response[idx++];
+            (*dataArrayP)[i].id += (((uint16_t)response[idx++]) << 8);
+            (*dataArrayP)[i].type = response[idx++];
+            len = response[idx++];
+            len += (((uint16_t)response[idx++]) << 8);
+            lwm2m_data_cp(&(*dataArrayP)[i], &response[idx], len);
+            idx += len;
+        }
+    }
+    response_free(context);
     return result;
 }
 
