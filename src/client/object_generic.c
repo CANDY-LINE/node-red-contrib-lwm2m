2ac15db6c7b94ddb969ba81728bad9e33e01a200
Daisuke Baba <baba.daisuke@gmail.com>
2017-10-13 15:08:01 +0900

    Add a new function to perform execute command

--- src/client/object_generic.c
+++ src/client/object_generic.c
@@ -459,9 +459,49 @@ static uint8_t prv_generic_execute(uint16_t instanceId,
                                    int length,
                                    lwm2m_object_t * objectP)
 {
-    uint8_t result = COAP_501_NOT_IMPLEMENTED;
+    uint16_t i = 0;
+    uint8_t messageId = 0x01;
+    uint8_t result;
     parent_context_t * context = (parent_context_t *)objectP->userData;
-    // TODO
+    size_t payloadRawLen = 10 + length;
+    uint8_t * payloadRaw = lwm2m_malloc(payloadRawLen);
+    payloadRaw[i++] = 0x01;                     // Data Type: 0x01 (Request), 0x02 (Response)
+    payloadRaw[i++] = messageId;                // Message Id associated with Data Type
+    payloadRaw[i++] = context->objectId & 0xff; // ObjectID LSB
+    payloadRaw[i++] = context->objectId >> 8;   // ObjectID MSB
+    payloadRaw[i++] = instanceId & 0xff;        // InstanceId LSB
+    payloadRaw[i++] = instanceId >> 8;          // InstanceId MSB
+    payloadRaw[i++] = 1;                        // # of resources LSB (always 01)
+    payloadRaw[i++] = 0;                        // # of resources MSB (always 00)
+    payloadRaw[i++] = resourceId & 0xff;        // ResourceId LSB
+    payloadRaw[i++] = resourceId >> 8;          // ResourceId MSB
+    memcpy(&payloadRaw[i], buffer, length);
+
+    fprintf(stderr, "prv_generic_execute:objectId=>%hu, instanceId=>%hu, resourceId=>%hu, buffer length=>%d\r\n",
+    context->objectId, instanceId, resourceId, length);
+    result = request_command(context, "execute", payloadRaw, payloadRawLen);
+    lwm2m_free(payloadRaw);
+
+    /*
+     * Response Data Format (result = COAP_NO_ERROR)
+     * 02 ... Data Type: 0x01 (Request), 0x02 (Response)
+     * 00 ... Message Id associated with Data Type
+     * 45 ... Result Status Code e.g. COAP_205_CONTENT
+     * 00 ... ObjectID LSB
+     * 00 ... ObjectID MSB
+     * 00 ... InstanceId LSB
+     * 00 ... InstanceId MSB
+     * 00 ... always 00
+     * 00 ... always 00
+     */
+    uint8_t * response = context->response;
+    if (COAP_NO_ERROR == result && response[0] == 0x02 && messageId == response[1]) {
+        result = response[2];
+    } else {
+        result = COAP_400_BAD_REQUEST;
+    }
+    response_free(context);
+    fprintf(stderr, "prv_generic_execute:result=>%u\r\n", result);
     return result;
 }
 
